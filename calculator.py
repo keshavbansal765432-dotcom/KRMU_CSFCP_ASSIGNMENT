e43289811a67b71d3ef1180c739de271351cbdc4
Keshav Bansal <keshav@kali>
2025-11-24 23:55:35 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,26 @@
+# Calculator - Mini Project
+
+print("----- Calculator -----")
+
+# Taking input from user
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Calculation
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Cannot divide by zero."
+else:
+    result = "Invalid operator!"
+
+# Showing result
+print("Result:", result)
